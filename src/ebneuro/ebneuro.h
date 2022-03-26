8416c4e4e006c9ec83597feeece49ae7a2982daa
Nikita Travkin <nikita@trvn.ru>
2022-03-26 12:54:49 +0500

    ebneuro: Add packet send/recv

---
+++ src/ebneuro/ebneuro.h
@@ -0,0 +1,24 @@
+#ifndef EBNEURO_H
+#define EBNEURO_H
+
+#include <stdint.h>
+
+#include "../system/system.h"
+#include "../system/endiannes.h"
+#include "../system/helpers.h"
+
+struct eb_dev {
+	int fd_init;
+	int fd_ctrl;
+	int fd_data;
+};
+
+/* network.c */
+int eb_send(int fd, uint8_t pid, const void *buf, uint16_t len);
+int eb_send_id(int fd, uint8_t pid);
+int eb_recv(int fd, void *buf, uint16_t len, int *err);
+int eb_recv_err(int fd);
+int eb_send_recv_err(int fd, uint8_t pid, const void *buf, uint16_t len);
+int eb_request_info(int fd, uint8_t pid, void *buf, uint16_t len, int *err)
+
+#endif /* EBNEURO_H */
---
+++ src/ebneuro/network.c
@@ -0,0 +1,141 @@
+
+/*
+ * network.c - messaging helpers.
+ */
+
+#include <stdint.h>
+#include <string.h>
+
+#include "../system/system.h"
+#include "../system/endiannes.h"
+#include "../system/helpers.h"
+
+#include "packets.h"
+#include "ebneuro.h"
+
+/**
+ * eb_send() - Send the request with payload.
+ * @fd:		Socket fd.
+ * @pid:	Packet ID.
+ * @buf:	Payload.
+ * @len:	Payload length.
+ *
+ * Return: As s_send.
+ */
+int eb_send(int fd, uint8_t pid, const void *buf, uint16_t len)
+{
+	int buflen = sizeof(struct eb_packet_hdr) +
+		     len + sizeof(uint8_t);
+	void *packet = malloc(buflen);
+	int ret;
+
+	s_dprintf(0, "%s: %d\n", __func__, buflen);
+
+	if (!packet) {
+		s_dprintf(CRITICAL,
+			  "%s: buffer allocation failed.\n", __func__);
+		return -12; // FIXME
+	}
+
+	((struct eb_packet_hdr*)packet)->magic = EB_PACKET_START_MAGIC;
+	((struct eb_packet_hdr*)packet)->id = pid;
+	((struct eb_packet_hdr*)packet)->length = cpu_to_be16(len);
+	if (buf)
+		memcpy(packet + sizeof(struct eb_packet_hdr), buf, len);
+	*(uint8_t*)(packet + sizeof(struct eb_packet_hdr) + len) = EB_PACKET_END_MAGIC;
+
+	ret = s_send(fd, packet, buflen, 0);
+	if (ret < 0)
+		s_dprintf(CRITICAL,
+			  "%s: packet send failure: %d\n", __func__, ret);
+	free(packet);
+	return ret;
+}
+
+/**
+ * eb_send_id() - send a request with no payload.
+ */
+int eb_send_id(int fd, uint8_t pid)
+{
+	return eb_send(fd, pid, NULL, 0);
+}
+
+/* FIXME: recv's don't expose packet id, but do we care? */
+
+/* FIXME: Does the EEG data packet have the error code?
+ * Maybe the err would read some garbae if not, can ignore that.
+ */
+
+/**
+ * eb_recv() - Receive a response from the device.
+ * @err:	Place to save returned error code, can be null.
+ */
+int eb_recv(int fd, void *buf, uint16_t len, int *err)
+{
+	int buflen = sizeof(struct eb_packet_hdr) +
+		     len + sizeof(__le16) + sizeof(uint8_t);
+	void *packet = malloc(buflen);
+	int ret;
+
+	if (!packet) {
+		s_dprintf(CRITICAL,
+			  "%s: buffer allocation failed.\n", __func__);
+		return -12; // FIXME
+	}
+
+	ret = s_recv(fd, packet, buflen, 0);
+	if (ret < 0) {
+		s_dprintf(CRITICAL,
+			  "%s: packet recv failure: %d\n", __func__, ret);
+		goto error;
+	}
+
+	buflen = be16_to_cpu(((struct eb_packet_hdr*)packet)->length);
+	if (buflen && buf)
+		memcpy(buf, packet + sizeof(struct eb_packet_hdr), buflen);
+	if (err)
+		*err = le16_to_cpu(*(__le16*)(packet+sizeof(struct eb_packet_hdr)+buflen));
+
+error:
+	free(packet);
+	return ret;
+}
+
+/**
+ * eb_recv_err() - Receive a response with no payload expected.
+ */
+int eb_recv_err(int fd)
+{
+	int ret, err;
+
+	ret = eb_recv(fd, NULL, 0, &err);
+	if (ret < 0)
+		return ret;
+
+	return err;
+}
+
+/**
+ * eb_send_recv_err() - Send message and expect empty return.
+ */
+int eb_send_recv_err(int fd, uint8_t pid, const void *buf, uint16_t len)
+{
+	int ret = eb_send(fd, pid, buf, len);
+	if (ret < 0)
+		return ret;
+
+	return eb_recv_err(dev->fd_init);
+}
+
+/**
+ * eb_request_info() - Send ID and get the payload.
+ */
+int eb_request_info(int fd, uint8_t pid, void *buf, uint16_t len, int *err)
+{
+	int ret = eb_send_id(fd, pid);
+	if (ret < 0)
+		return ret;
+
+	return eb_recv(fd, buf, len, err);
+}
+
