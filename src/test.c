f6ce94759d025acb8d33a5a1567d65475d4f9c95
Nikita Travkin <nikita@trvn.ru>
2022-04-21 11:37:33 +0500

    test: read impedance

--- src/test.c
+++ src/test.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		.data_rate = 512,
 	};
 
-	int ret, i, j;
+	int ret, i, j, k;
 	ret = eb_prepare(&dev);
 	s_dprintf(ALWAYS, "prep -> %d\n", ret);
 
@@ -49,8 +49,30 @@ int main(int argc, char *argv[])
 		s_dprintf(ALWAYS, "\n");
 	}
 
-	ret = eb_set_mode(&dev, EB_MODE_IDLE);
+	ret = eb_set_mode(&dev, EB_MODE_IMPEDANCE);
 	s_dprintf(ALWAYS, "set preset -> %d\n", ret);
+
+	short eeg_imp[64 * 1] = {0};
+	short dc_imp[4 * 1] = {0};
+	for (i = 0; i < 5; ++i) {
+		ret = eb_get_impedances(&dev, &eeg_imp[0], &dc_imp[0]);
+		s_dprintf(ALWAYS, "%5d; %5d\n", i, ret);
+
+		for (j = 0; j < 8; ++j) {
+			for (k = 0; k < 8; ++k)
+				s_dprintf(ALWAYS, "%8d ", eeg_imp[j*8 + k]);
+
+			s_dprintf(ALWAYS, "\n");
+		}
+
+		s_dprintf(ALWAYS, "\n");
+		for (j = 0; j < 4; ++j)
+			s_dprintf(ALWAYS, "%8d ", dc_imp[j]);
+
+
+		s_dprintf(ALWAYS, "\n--------------------\n");
+	}
+
 
 	ret = eb_unprepare(&dev);
 	s_dprintf(ALWAYS, "unp -> %d\n", ret);
