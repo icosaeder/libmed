c61167f6e5fe5782a0695d797039d9316f9715eb
Nikita Travkin <nikita@trvn.ru>
2022-04-21 12:23:11 +0500

    test: print all info in matricies

--- src/test.c
+++ src/test.c
@@ -37,16 +37,19 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < 20; ++i) {
 		ret = eb_get_data(&dev, eeg_data, dc_data, 1);
-		s_dprintf(ALWAYS, "%5d; %5d) ", i, ret);
-		for (j = 0; j < 10; ++j)
-			s_dprintf(ALWAYS, "%8.2f ", eeg_data[j]);
+		s_dprintf(ALWAYS, "%5d; %5d) \n", i, ret);
+		for (j = 0; j < 8; ++j) {
+			for (k = 0; k < 8; ++k)
+				s_dprintf(ALWAYS, "%8.2f ", eeg_data[j*8 + k]);
+			s_dprintf(ALWAYS, "\n");
+		}
 
-		s_dprintf(ALWAYS, " | ");
+		s_dprintf(ALWAYS, "\n");
 
 		for (j = 0; j < 4; ++j)
 			s_dprintf(ALWAYS, "%9.2f ", dc_data[j]);
 
-		s_dprintf(ALWAYS, "\n");
+		s_dprintf(ALWAYS, "\n--------------------\n");
 	}
 
 	ret = eb_set_mode(&dev, EB_MODE_IMPEDANCE);
@@ -54,7 +57,7 @@ int main(int argc, char *argv[])
 
 	short eeg_imp[64 * 1] = {0};
 	short dc_imp[4 * 1] = {0};
-	for (i = 0; i < 5; ++i) {
+	for (i = 0; i < 20; ++i) {
 		ret = eb_get_impedances(&dev, &eeg_imp[0], &dc_imp[0]);
 		s_dprintf(ALWAYS, "%5d; %5d\n", i, ret);
 
