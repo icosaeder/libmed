7f7b16c302c46905381c9e3fe8e2fcf2f2341844
Nikita Travkin <nikita@trvn.ru>
2023-01-26 17:55:24 +0500

    system: add container_of
    Signed-off-by: Nikita Travkin <nikita@trvn.ru>

--- src/system/include/system/helpers.h
+++ src/system/include/system/helpers.h
@@ -7,6 +7,11 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#include <stddef.h>
+
 #define BIT(x) (1 << x)
+
+#define container_of(ptr, type, member) \
+	((type *)((void*)(ptr) - offsetof(type, member)))
 
 #endif /* HELPERS_H */
